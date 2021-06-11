2ff3ebe939bfebe7be708d35366a8faca6bea4e1
reeceawalsh <reeceawalsh@hotmail.co.uk>
2021-06-11 20:15:41 +0800

    Neated up the javascript and removed excess lines
    Still need to find a away around naming the nav bar id's '1','2','3'

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ const tabItems = document.querySelectorAll(".tab-item");
 const tabContentItems = document.querySelectorAll(".tab-content-item");
 const navLinks = document.querySelectorAll(".nav-link");
 
-console.log(tabItems);
-console.log(tabContentItems);
-console.log(navLinks);
-
 // Select tab content item
 function selectItem(e) {
   // Remove all show and border classes
   removeShow();
   removeBorder();
   // Add border to current tab item
-  this.classList.add("tab-border");
+  const tabItems = document.querySelector(`#tab-${this.id}`);
+  tabItems.classList.add("tab-border");
   // Grab content item from DOM
-  const tabContentItem = document.querySelector(`#${this.id}-content`);
-
-  console.log(tabContentItem);
+  const tabContentItem = document.querySelector(`#tab-${this.id}-content`);
   //Add show class
   tabContentItem.classList.add("show");
 }
@@ -39,17 +34,7 @@ tabItems.forEach((item) => {
   item.addEventListener("click", selectItem);
 });
 
-function selectNavItem(e) {
-  removeShow();
-  removeBorder();
-  const tabContentItem = document.querySelector(`#tab-${this.id}-content`);
-  const tabItems = document.querySelector(`#tab-${this.id}`);
-  tabItems.classList.add("tab-border");
-  tabContentItem.classList.add("show");
-}
-// List for nav item click
+// Listen for nav item click
 navLinks.forEach((item) => {
-  item.addEventListener("click", selectNavItem);
+  item.addEventListener("click", selectItem);
 });
-
-// Click on the nav link, it runs the selectItem function on the tab-content using the ID of the nav link.
